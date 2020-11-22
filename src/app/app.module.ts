e7554afb66887609434c5c38bb400163496c1fff
EDUCANETstefan <EDUCANETstefan>
2020-11-22 11:04:37 +0100

    HOTOVO MARŤO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {FormsModule} from "@angular/forms";
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+
+interface iSearch {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  searchLink = '';
+
+  linksArray: iSearch[] = [
+    { title: "home", link: "home" },
+    { title: "about", link: "about" },
+    { title: "contact", link: "contact"}
+  ];
+
+  filter(): iSearch[] {
+    const result: iSearch[] = [];
+    for (const option of this.linksArray) {
+      if (option.title.includes(this.searchLink)) {
+        result.push(option);
+      }
+    }
+    return result;
+  }
+
+  @Output()
+  clicked = new EventEmitter<void>();
+
+}
